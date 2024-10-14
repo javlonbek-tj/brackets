6770b2d39e6b64915eccd9f1579f321cfcd11041
Жавлон Турдиматов <j.turdimatov@dkp.local>
2024-10-14 20:02:41 +0500

    feat: add brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const openBrackets = [];
+  const bracketsPair = {};
+  bracketsConfig.forEach((bracketC) => {
+    openBrackets.push(bracketC[0]);
+    bracketsPair[bracketC[1]] = bracketC[0];
+  });
+
+  const stack = [];
+
+  for (let i = 0; i < str.length; i += 1) {
+    const currentSymbol = str[i];
+
+    if (openBrackets.includes(currentSymbol)) {
+      if (
+        bracketsPair[currentSymbol] === currentSymbol &&
+        stack.length > 0 &&
+        stack[stack.length - 1] === currentSymbol
+      ) {
+        stack.pop();
+      } else {
+        stack.push(currentSymbol);
+      }
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+
+      const topElement = stack[stack.length - 1];
+
+      if (bracketsPair[currentSymbol] === topElement) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
